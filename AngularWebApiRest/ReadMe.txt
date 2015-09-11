f508d4d5b20ad9f75cdc54edbbc6a28a9847d19d
Sandra Basquero <sandrabasquero@gmail.com>
2015-09-11 12:44:02 +0200

    First commit
    ToDo list test project

---
+++ AngularWebApiRest/ReadMe.txt
@@ -0,0 +1,5 @@
+Read Me
+
+Hay que tener instalado y arrancado mongod
+Luego ejecutar el fichero server.js con node -> en terminal: "node server.js"
+En el navegador -> http://localhost:8080
---
+++ AngularWebApiRest/public/main.js
@@ -0,0 +1,68 @@
+angular.module('angularTodo', []);  
+
+function mainController($scope, $http) {
+    $scope.formData = {};
+
+    // Cuando se cargue la página, pide del API todos los TODOs
+    $http.get('/api/todos')
+        .success(function(data) {
+            $scope.tareas = data;
+            console.log(data)
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // Cuando se añade un nuevo TODO, manda el texto a la API
+    $scope.createTodo = function(){
+        $scope.formData.saveDate = Date(); //set date data
+
+        console.log("----------- ");
+        console.log($scope.formData.saveDate);
+        //Envio del dato a la bbdd a través de la API, utilizando las rutas, que para eso están.
+        $http.post('/api/todos', $scope.formData)
+        	//si se añade con éxito, pintar el json creado en la pantalla
+            .success(function(data) {
+                $scope.formData = {}; //limpiar formData una vez añadido a la bd
+                $scope.tareas = data;
+            })
+            //si da error, imprimir por consola un mensaje
+            .error(function(data) {
+                console.log('Error:' + data);
+            });
+    };
+
+    // Borra un TODO despues de checkearlo como acabado
+    $scope.deleteTodo = function(id) {
+        $http.delete('/api/todos/' + id)
+            .success(function(data) {
+                $scope.tareas = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error:' + data);
+            });
+    };
+
+
+    //************************************************************
+    //************************************************************
+
+    function nowDate() {
+        var fullDate = Date();
+        var day = fullDate.getDate();
+        var month = fullDate.getMonth() + 1;
+        var year = fullDate.getFullYear();
+
+        var stringDate = day + "/" + month + "/" + year;
+
+        return stringDate;
+    }
+
+}
+
+
+
+
+
+
